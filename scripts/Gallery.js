3e702c12fa3b33ac4a31507400137c4bcb6bbe87
DanielS6 <Daniel@nevet.com>
2023-03-01 19:26:37 -0500

    Save work from end of meeting 2023.03.01

--- scripts/Gallery.js
+++ scripts/Gallery.js
@@ -4,25 +4,28 @@
 window.addEventListener( 'load', function () {
 
     // elements
-    const $img = $('#slideshow-image')
+    const $imgs = $('#gallery-wrapper img')
+    // Access the three individual images to change the pictures shown
+    const $img1 = $('#slideshow-image-left');
+    const $img2 = $('#slideshow-image-mid');
+    const $img3 = $('#slideshow-image-right');
+
     const $startBtn = $('#slideshow-start');
     const $stopBtn = $('#slideshow-stop');
 
     // Image sources
     const imgURLs = [
-        './images/catprofile.jpg',
-        './images/dogprofile.jpg',
-        './images/catprofile2.jpg',
-        './images/dogprofile5.jpg',
-        './images/catprofile2.jpg',
-        './images/dogprofile3.jpg'
+        [ './images/cat3.jpg', './images/dog3.jpg', './images/cat4.jpg' ],
+        [ './images/dog4.jpg', './images/cat5.jpg', './images/dog5.jpg' ],
+        [ './images/cat6.jpg', './images/dog6.jpg', './images/cat7.jpg' ],
+        [ './images/dog7.jpg', './images/cat8.jpg', './images/dog8.jpg' ]
     ];
-    // HTML is coded to start with catprofile.jpg
+    // HTML is coded to start with the first set of images.jpg
     let currImgIdx = 0;
     let slideshowRunning = false;
 
     // Milliseconds for fades
-    const SLIDESHOW_SPEED = 1000;
+    const SLIDESHOW_SPEED = 2500;
 
     // Forward declare because of mutual recursion
     let afterFadeIn;
@@ -30,13 +33,15 @@ window.addEventListener( 'load', function () {
         // Stopping the slideshow means that the next image won't fade out,
         // but we will still finish fading in the current one
         currImgIdx = (currImgIdx + 1) % imgURLs.length;
-        $img.attr('src', imgURLs[currImgIdx]);
-        $img.fadeIn(SLIDESHOW_SPEED, afterFadeIn);
+        $img1.attr('src', imgURLs[currImgIdx][0]);
+        $img2.attr('src', imgURLs[currImgIdx][1]);
+        $img3.attr('src', imgURLs[currImgIdx][2]);
+        $imgs.fadeIn(SLIDESHOW_SPEED, afterFadeIn);
     };
     afterFadeIn = () => {
         // Don't continue if the slideshow is stopped
         if (slideshowRunning) {
-            $img.fadeOut(SLIDESHOW_SPEED, afterFadeOut);
+            $imgs.fadeOut(SLIDESHOW_SPEED, afterFadeOut);
         }
     };
 
