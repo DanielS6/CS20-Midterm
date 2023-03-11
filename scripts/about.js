a6bf1a39aab206413f582f714fd2b07691ac68ae
DanielS6 <Daniel@nevet.com>
2023-03-10 19:33:50 -0500

    Save work from meeting 2023.03.10
    We will keep working but just to have a breakpoint in the history

--- scripts/about.js
+++ scripts/about.js
@@ -1,6 +1,16 @@
-$(document).ready(function() {
-    $("about").hover(function() {
-        var imgNum = Math.floor((Math.random()*8) + 2); 
-        $(".about").css("background-image", "url(./cat0)");
-    })
+var pets = ["dog", "cat"];
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("box0").backgroundColor = "#f3f3f3"
+    var imgNum = 2;
+    for (i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * pets.length);
+        var pet = pets[randomIndex];
+        document.getElementById("box" + i).style.backgroundImage = "url('./images/" + pet + imgNum + ".jpg')"
+        document.getElementById("box" + i).style.setProperty("background-position", "50% 40%");
+        document.getElementById("box" + i).style.setProperty("background-repeat", "no-repeat")
+        document.getElementById("box" + i).style.setProperty("background-size", "cover")
+        imgNum++
+    }
+
 });
